164547b7d336f406cfd2262b1b4ebe8851072b38
JackyChn <ccc1020048076@gmail.com>
2024-09-07 01:25:48 +0930

    Add: Counter test (deleted), loading test. Start to build the app

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,5 +1,21 @@
-function page() {
-  return <div>Cabin page</div>;
+import Counter from "../components/Counter";
+
+async function page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return (
+    <div>
+      <h1>Cabin page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id} className="list-none">
+            {user.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default page;
