b6f7be9236f47d1bcd858c122b1920d70844726b
JackyChn <ccc1020048076@gmail.com>
2024-09-10 20:53:18 +0930

    Add: replace cabin page url by adding handleFilter() so that cabinList will be updated accourdingly

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -1,16 +1,44 @@
 "use client";
 
+import { usePathname, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+
 function Filter() {
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const handleFilter = (capacity) => {
+    const params = new URLSearchParams(searchParams);
+    console.log(params);
+    params.set("capacity", capacity);
+    router.replace(`${pathName}?${params.toString()}`, { scroll: false });
+  };
+
   return (
     <div className="border border-primary-800 flex">
-      <button className="px-5 py-2 hover:bg-primary-700">All Cabins</button>
-      <button className="px-5 py-2 hover:bg-primary-700">
+      <button
+        className="px-5 py-2 hover:bg-primary-700"
+        onClick={() => handleFilter("all")}
+      >
+        All Cabins
+      </button>
+      <button
+        className="px-5 py-2 hover:bg-primary-700"
+        onClick={() => handleFilter("small")}
+      >
         1&mdash;3 gurests
       </button>
-      <button className="px-5 py-2 hover:bg-primary-700">
+      <button
+        className="px-5 py-2 hover:bg-primary-700"
+        onClick={() => handleFilter("medium")}
+      >
         4&mdash;7 gurests
       </button>
-      <button className="px-5 py-2 hover:bg-primary-700">
+      <button
+        className="px-5 py-2 hover:bg-primary-700"
+        onClick={() => handleFilter("large")}
+      >
         8&mdash;12 gurests
       </button>
     </div>
