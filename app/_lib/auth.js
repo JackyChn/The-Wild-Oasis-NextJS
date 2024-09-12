4dc310e4e0ede156f917b63ba4c4b5100e2dca81
JackyChn <ccc1020048076@gmail.com>
2024-09-13 05:11:08 +0930

    Add: Signout fuction in sidebar

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -12,6 +13,23 @@ const authConfig = {
     authorized({ auth, req }) {
       return !!auth?.user;
     },
+    // async signIn({ user, account, profile }) {
+    //   try {
+    //     const existGuest = await getGuest(user.email); // fixed typo here
+    //     if (!existGuest) {
+    //       await createGuest({ email: user.email, fullName: user.name });
+    //     }
+    //     return true;
+    //   } catch (error) {
+    //     console.error("Error during signIn:", error); // added error logging
+    //     return false;
+    //   }
+    // },
+    // async session({ session, user }) {
+    //   const guest = await getGuest(session.user.email);
+    //   session.user.guestId = guest.id; // assing a global var: guestId and sotre it in session, so it can be used all over the place
+    //   return session;
+    // },
   },
   pages: {
     signIn: "/login",
